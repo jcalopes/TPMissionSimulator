03be450b62f522295ffc93b6fbbd07348c111d40
joaoLopes <lopes.jonas.7@gmail.com>
2021-02-22 01:51:04 +0000

    Export Manual Simulation

---
+++ MissionsSimulator/src/exceptions/NoManualSimulationsException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exceptions;
+
+/**
+ * 
+ */
+public class NoManualSimulationsException extends Exception{
+    public NoManualSimulationsException(String msg){
+        super(msg);
+    }
+}
---
+++ MissionsSimulator/src/readWriteJson/ExporterData.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package readWriteJson;
+
+import exceptions.NoManualSimulationsException;
+import exceptions.NullElementValueException;
+import interfaces.IDivision;
+import interfaces.IManualSimulation;
+import interfaces.IVersion;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+ * 
+ */
+public class ExporterData {
+    
+    public static String exportSimulacoesManuais(String codMission, IVersion version) 
+            throws NullElementValueException, NoManualSimulationsException, IOException {
+        if(codMission==null || version==null)
+            throw new NullElementValueException("Invalid Arguments");
+        if(version.getNumManualSimulations()==0){
+            throw new NoManualSimulationsException("There are not manual simulations");
+        }
+        JSONObject jDoc = new JSONObject();
+        jDoc.put("CodMissao", codMission);
+        jDoc.put("Versao", version.getCodVersion());
+        jDoc.put("NumSimulacoesManuais", version.getNumManualSimulations());
+        JSONArray jSimulacoes=new JSONArray();
+        
+        Iterator<IManualSimulation> simulacoes=version.getManualSimulation().iterator();
+        
+        while(simulacoes.hasNext()){
+            IManualSimulation current=simulacoes.next();
+            JSONObject jSimulacaoManual=new JSONObject();
+            jSimulacaoManual.put("Sucesso", current.isSuccess());
+            jSimulacaoManual.put("PontosVida",current.getRemainingLife());
+            if(!current.hasRecoverDamage()){
+                jSimulacaoManual.put("PowerUp", "Recover Last Damage");
+            }
+
+            if (!current.hasRestoreLife()) {
+                jSimulacaoManual.put("PowerUp", "Restore Life");
+            }    
+            
+            JSONArray trajetoPercorrido=new JSONArray();
+            Iterator<IDivision> divisoes=current.getPath().iterator();
+            
+            while(divisoes.hasNext()){
+                trajetoPercorrido.add(divisoes.next().getName());
+            }
+            jSimulacaoManual.put("Trajeto",trajetoPercorrido);
+            jSimulacoes.add(jSimulacaoManual);
+        }
+
+        jDoc.put("Simulacoes", jSimulacoes);
+
+        try (FileWriter file = new FileWriter("Relatorios/" + "CodMissao_"+codMission+
+                "Versao_"+version.getCodVersion()+".json")) {
+            file.write(jDoc.toJSONString());
+        }
+        String result = jDoc.toJSONString();
+        return result;
+    }
+}
